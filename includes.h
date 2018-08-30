d7be82e253a337d4e6e7e53a2735c43560e1db56
Matt <matt@snoopy.cyrusglobal>
2018-08-29 20:59:08 -0400

    Initial load

---
+++ includes.h
@@ -0,0 +1,52 @@
+#ifdef _WIN32
+   //define something for Windows (32-bit and 64-bit, this part is common)
+   #ifdef _WIN64
+      //define something for Windows (64-bit only)
+   #else
+      //define something for Windows (32-bit only)
+   #endif
+#elif __APPLE__
+    #include "TargetConditionals.h"
+    #if TARGET_IPHONE_SIMULATOR
+         // iOS Simulator
+    #elif TARGET_OS_IPHONE
+        // iOS device
+    #elif TARGET_OS_MAC
+        // Other kinds of Mac OS
+    #else
+    #   error "Unknown Apple platform"
+    #endif
+#elif __linux__
+    // linux
+#elif __unix__ // all unices not caught above
+    // Unix
+#elif defined(_POSIX_VERSION)
+    // POSIX
+#else
+#   error "Unknown compiler"
+#endif
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+#include <unistd.h>
+
+using namespace std;
+
+class dbase {
+
+public:
+    void dbmain();
+
+    // Default Constructor
+    dbase()
+    {
+        quit = 0;
+    }
+
+    int quit;
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "includes.h"
+
+using namespace std;
+
+dbase db;  // class instance
+
+int main()
+{
+    db.dbmain();
+    return 0;
+}
